787e6fbd7cef8f84f7860c2abfbfd3d24718131b
roydonGuo <3133010060@qq.com>
2023-09-12 18:01:48 +0800

    utils

---
+++ leadnews-utils/src/main/java/com/roydon/utils/thread/WmThreadLocalUtil.java
@@ -0,0 +1,24 @@
+package com.roydon.utils.thread;
+
+import com.roydon.model.wemedia.pojo.WmUser;
+
+public class WmThreadLocalUtil {
+
+    private final static ThreadLocal<WmUser> WM_USER_THREAD_LOCAL = new ThreadLocal<>();
+
+    //存入线程中
+    public static void setUser(WmUser wmUser) {
+        WM_USER_THREAD_LOCAL.set(wmUser);
+    }
+
+    //从线程中获取
+    public static WmUser getUser() {
+        return WM_USER_THREAD_LOCAL.get();
+    }
+
+    //清理
+    public static void clear() {
+        WM_USER_THREAD_LOCAL.remove();
+    }
+
+}
