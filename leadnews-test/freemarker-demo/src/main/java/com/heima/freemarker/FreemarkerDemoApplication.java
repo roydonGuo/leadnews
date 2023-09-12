bc54f773b24d4ad6401901486eb43ca04a1ccfcb
roydonGuo <3133010060@qq.com>
2023-09-12 18:02:22 +0800

    freemarker与minio测试

---
+++ leadnews-test/freemarker-demo/src/main/java/com/heima/freemarker/FreemarkerDemoApplication.java
@@ -0,0 +1,12 @@
+package com.heima.freemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreemarkerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreemarkerDemoApplication.class,args);
+    }
+}
