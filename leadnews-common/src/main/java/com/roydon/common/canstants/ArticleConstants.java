d70aa789156b607962c877219cd8f71596301872
roydonGuo <3133010060@qq.com>
2023-09-12 18:01:28 +0800

    常量配置

---
+++ leadnews-common/src/main/java/com/roydon/common/canstants/ArticleConstants.java
@@ -0,0 +1,9 @@
+package com.roydon.common.canstants;
+
+public class ArticleConstants {
+
+    public static final Short LOADTYPE_LOAD_MORE = 1;
+    public static final Short LOADTYPE_LOAD_NEW = 2;
+    public static final String DEFAULT_TAG = "__all__";
+
+}
